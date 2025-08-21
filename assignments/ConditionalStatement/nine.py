338f8be8a7c2f1bc94efe8edc02f72edb4cccd68
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-21 16:36:51 +0530

    Python

---
+++ assignments/ConditionalStatement/nine.py
@@ -0,0 +1,7 @@
+# 9. Program to check if a number is a 3-digit number or not?
+num=int(input("enter the digit:"))
+# for num in range(100,999):
+if num in range(100,999):
+    print("Entered number is is three-digit number")
+else:
+    print("not a three digit number")
