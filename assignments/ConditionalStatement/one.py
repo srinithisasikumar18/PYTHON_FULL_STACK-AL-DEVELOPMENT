5f8e8707c741460036df5926937f5ab90c5d73da
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-21 15:36:30 +0530

    ConditionalStatement Assignment

---
+++ assignments/ConditionalStatement/one.py
@@ -0,0 +1,5 @@
+# Write a program to read 2 numbers from the keyboard and print sum?
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+sum=num1+num2
+print(sum)
