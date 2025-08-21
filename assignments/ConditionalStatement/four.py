f7bce85ce1e88b2afc4cb623dc940d27817019b0
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-21 15:50:33 +0530

    ConditionalStatement Assignment

---
+++ assignments/ConditionalStatement/four.py
@@ -0,0 +1,6 @@
+# 4. Program to check if a number is multiple of 3 not not?
+num=int(input("enter the number:"))
+if num%3==0:
+    print(num,"is divisible by Three")
+else:
+    print(num,"is not divisible by Three")
