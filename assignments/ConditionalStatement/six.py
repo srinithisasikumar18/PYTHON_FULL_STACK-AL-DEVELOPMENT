c42ec47bc40446ea649a0eff0e931637b5feb504
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-21 16:16:49 +0530

    Python

---
+++ assignments/ConditionalStatement/six.py
@@ -0,0 +1,15 @@
+# 6. Write a program to take single digit number from the keyboard and print in english?
+num=int(input("enter a single digit:"))
+m={
+    1:"one",
+    2:"two",
+    3:"three",
+    4:"four",
+    5:"five",
+    6:"six",
+    7:"seven",
+    8:"eight",
+    9:"nine"
+}
+
+print(m[num])
