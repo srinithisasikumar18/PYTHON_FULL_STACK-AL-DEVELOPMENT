ec1c9a9b3773f91734de9c472bcdbc19bb2bc42c
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-21 16:24:56 +0530

    Python

---
+++ assignments/ConditionalStatement/seven.py
@@ -0,0 +1,10 @@
+# 7. Write a program to find the biggest of given 2 numbers from the command
+# prompt?
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+if num1>num2:
+    print("num1 is bigger")
+elif num1==num2:
+    print("num1 and num2 are equal")
+else:
+    print("num2 is bigger")
