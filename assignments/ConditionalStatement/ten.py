ce872854210e176898c5d3e017ea38cf008846bd
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-08-21 16:40:04 +0530

    Python

---
+++ assignments/ConditionalStatement/ten.py
@@ -0,0 +1,6 @@
+# 10.Program to print even or odd for a given number from CMD/CLA?
+num=int(input("enter the number"))
+if num%2==0:
+    print(num, "is even number")
+else:
+    print(num,"is odd numer")
